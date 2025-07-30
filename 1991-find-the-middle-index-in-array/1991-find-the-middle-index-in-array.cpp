9c65a6315b8c860c5ee95e51b1c6d175bcfe8da5
Santosh <savaramsantosh18@gmail.com>
2025-07-30 15:25:50 +0530

    Time: 0 ms (100%), Space: 16.8 MB (9.12%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int n=nums.size();
+        int i;
+        vector<int>le(n);
+         vector<int>re(n);
+          le[0]=0;
+          re[n-1]=0;
+     for(i=1;i<n;i++) 
+        {
+        le[i]=le[i-1]+nums[i-1];
+        }
+        for(i=n-2;i>=0;i--) 
+        {
+        re[i]=re[i+1]+nums[i+1];
+        }
+        for(i=0;i<n;i++)
+        {
+            if(re[i]==le[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
