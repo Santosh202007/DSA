84e55d20886ba8135b3f09c17192b4ffaf63539e
Santosh <savaramsantosh18@gmail.com>
2026-03-23 20:00:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,18 +5,18 @@ public:
         while(l<=r)
         {
             int mid=l+(r-l)/2;
-            if(arr[mid-1]<=arr[mid] && arr[mid]>=arr[mid+1])
-            {
-                return mid;
-            }
-            else if(arr[mid]>arr[mid-1])
-            {
-                l=mid;
-            }
-            else
-            {
-                r=mid;
-            }
+ if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
+ {
+    return mid;
+ }
+ if(arr[mid]>arr[mid-1])
+ {
+l=mid+1;
+ }
+ else
+ {
+r=mid-1;
+ }
         }
         return -1;
     }
